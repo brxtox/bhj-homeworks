f815323e3caacd0bc2d794d4aa357f00d6cc4902
Abdellah Shenni <brxtox@gmail.com>
2025-03-05 22:06:33 +0300

    first commit countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,12 @@
+const timerElement = document.getElementById("timer");
+
+function countDown() {
+  let time = parseInt(timerElement.textContent, 10);
+  time--;
+  timerElement.textContent = time;
+  if (time === 0) {
+    clearInterval(countDownTimer);
+    alert('Вы победили в конкурсе!');
+  }
+}
+const countDownTimer = setInterval(countDown, 1000);
